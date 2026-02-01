41eb8d1bf4dac23fdb4383786aeec28a243d0164
MamedliFidan <mfidan742@gmail.com>
2026-02-01 21:34:43 +0400

    changed

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
- //1
- document.addEventListener("keydown",(e)=> {
-      if (e.key === "Enter") {
-        console.log("Enter");
-      }
-    });
- //2
- let inp_name=document.querySelector(".name");
- let inp_comment=document.querySelector(".comment");
- let button=document.querySelector(".button2")
- button.addEventListener("click",()=>{
-    console.log("Name: "+inp_name.value,"Comment: "+inp_comment.value);
- })
- 
- 
- //3
- const form = document.querySelector("form");
-  const passwordInput = document.querySelector(".in3");
+let next = document.querySelector(".next");
+let back = document.querySelector(".back");
+let img = document.querySelector("div img");
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+let list = [
+  "https://i.pinimg.com/1200x/26/73/38/267338dab242a897c07f0ab8e3815f0b.jpg",
+  "https://i.pinimg.com/1200x/f9/d5/12/f9d51231950d9f037b86948f21b7dc69.jpg",
+  "https://i.pinimg.com/1200x/15/b2/cb/15b2cb4544ef38e73c02b3aff2b23508.jpg",
+  "https://i.pinimg.com/1200x/ac/98/33/ac9833eb194f46c1c81fd71405c21ec0.jpg"
+];
 
-    const password = passwordInput.value;
+let i = 0;
+img.src = list[i]; // первая картинка
 
-    if (password.length >= 5) {
-      console.log("enough");
-    } else {
-      console.log("more");
-    }
-  });
+next.addEventListener("click", () => {
+  i++;
+  if (i >= list.length) {
+    i = 0;
+  }
+  img.src = list[i];
+});
+back.addEventListener("click", () => {
+  i--;
+  if (i <0) {
+    i = list.length-1;
+  }
+  img.src = list[i];
+});
